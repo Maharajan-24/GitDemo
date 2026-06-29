69f8217acec39aae2c7867d49f6e513663804d16
raja <sudalairaj702@gmail.com>
2026-06-29 19:08:17 +0530

    update

---
+++ src/test/java/capgemini/waltdisney/ExtentReport.java
@@ -0,0 +1,38 @@
+package capgemini.waltdisney;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReport {
+
+	ExtentReports extent;
+
+	@Test
+	public void getlink() {
+		ExtentTest test = extent.createTest("Getting link");// create a test
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/");
+		System.out.println(driver.getTitle());
+		extent.flush();// saying test is over
+	}
+
+	@BeforeTest
+	public void report() {
+		String path = System.getProperty("user.dir") + "\\reports\\index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);// here we need togive path where report should
+																		// save so this will create a report in that
+																		// purticular path
+		reporter.config().setReportName("dummyresults");
+		reporter.config().setDocumentTitle("summa");
+
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Raja");
+	}
+}
